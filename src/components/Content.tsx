236e2bb43c58dbb49f3100f3c4fe50db11e42a13
lucas bandeira <lucas4workl8tr@gmail.com>
2021-10-25 10:23:50 -0300

    desafio completo

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,56 +1,22 @@
 import {MovieCard} from "./MovieCard";
-import {useEffect, useState} from "react";
-import {api} from "../services/api";
-
 import '../styles/content.scss';
+import {GenreResponseProps, MovieProps} from "../App";
 
-interface GenreResponseProps {
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
+interface IContentProps {
+    selectedGenre: GenreResponseProps,
+    movies: MovieProps[];
 }
 
-interface MovieProps {
-    imdbID: string;
-    Title: string;
-    Poster: string;
-    Ratings: Array<{
-        Source: string;
-        Value: string;
-    }>;
-    Runtime: string;
-}
+export function Content(props: IContentProps) {
 
-export function Content() {
-    const [selectedGenreId, setSelectedGenreId] = useState(1);
+    const {selectedGenre, movies} = props;
 
-    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-    const [movies, setMovies] = useState<MovieProps[]>([]);
-    const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
-    useEffect(() => {
-        api.get<GenreResponseProps[]>('genres').then(response => {
-            setGenres(response.data);
-        });
-    }, []);
-
-    useEffect(() => {
-        api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-            setMovies(response.data);
-        });
-
-        api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-            setSelectedGenre(response.data);
-        })
-    }, [selectedGenreId]);
-
-  return (
-      <>
-          <div className="container">
-              <header>
-                  <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-              </header>
+    return (
+        <>
+            <div className="container">
+                <header>
+                    <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+                </header>
 
               <main>
                   <div className="movies-list">
